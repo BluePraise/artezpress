8d62ea226e5016f9cc58876a18d5610f734a6c83
Magalie Linda <magaliechetrit@gmail.com>
2020-08-20 11:08:10 +0200

    header bottom fixed

--- header.php
+++ header.php
@@ -1,17 +1,7 @@
-<!DOCTYPE html>
+<?php 
+ 	get_template_part( 'head' );
+?>
 
-<html class="no-js" <?php language_attributes(); ?>>
-
-	<head>
-
-		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
-
-		<link rel="profile" href="https://gmpg.org/xfn/11">
-
-		<?php wp_head(); ?>
-
-	</head>
-
-	<body <?php body_class(); ?>>
-
+<header class="fixed-bottom">
+	<h1>IK BEN EEN HEADER</h1>
+</header>
