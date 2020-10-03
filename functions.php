b0a2e9ce32909d21553621388fb6c406c96d8c35
Magalie Linda <magaliechetrit@gmail.com>
2020-10-03 12:24:09 +0200

    editor styling and styling of the edit

--- functions.php
+++ functions.php
@@ -85,3 +85,11 @@ if ( ! function_exists( 'woocommerce_widget_shopping_cart_subtotal' ) ) {
 	}
 }
 
+add_action( 'after_setup_theme', 'artezpress_gutenberg_css' );
+ 
+function artezpress_gutenberg_css(){
+ 
+	add_theme_support( 'editor-styles' ); // if you don't add this line, your stylesheet won't be added
+	add_editor_style( 'style-editor.css' ); // tries to include style-editor.css directly from your theme folder
+ 
+}
