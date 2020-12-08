4fbf346904f439d9a278f4bdfb409efe9d47047e
Magalie Linda <magaliechetrit@gmail.com>
2020-12-08 20:44:54 +0100

    Random Image in Handshake is now functioning including User Roles

--- blocks/frontpage/handshake/index.php
+++ blocks/frontpage/handshake/index.php
@@ -1,1 +1,10 @@
-<h1>ArtEZ Press</h1>
+<?php 
+    $rows = get_field('hero_bg_imgs'); // get all the rows 
+    $rand_row = $rows[ array_rand( $rows ) ];
+    $rand_row_image = $rand_row['bg_images_uploaded']; // get the sub field value 
+?>
+
+<div class="hero handshake" style="background-image: url(<?php echo $rand_row_image; ?>);">
+
+    <h1>ArtEZ Press</h1>
+</div>
