20455b6ecce507b2367737de5efcd034d35d018f
Magalie Linda <magaliechetrit@gmail.com>
2021-01-12 17:49:20 -0500

    WIP: Video module missing #28

--- blocks/postbuilding/index.php
+++ blocks/postbuilding/index.php
@@ -18,6 +18,20 @@ if( have_rows('post_building_modules') ):
             <a href="<?php echo $book_url; ?>">Read More</a>
         <?php endif; 
 
+        if( get_row_layout() == 'video_module' ):
+            $video_url      = get_sub_field('video_url');
+            $video_caption  = get_sub_field('video_caption');
+            ?>
+            
+            <?php if ($video_url): ?>
+                <div class="video-container"><?php echo $video_url; ?></div>
+            <?php endif; ?>
+
+            <?php if ($video_caption): ?>
+                <p><?php echo $video_caption; ?></p>
+            <?php endif; ?>
+        <?php endif; 
+
         // // Case: Download layout.
         // elseif( get_row_layout() == 'download' ): 
         //     $file = get_sub_field('file');
