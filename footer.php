fd0365fda236603ffd041e5b17c832cd83d3c801
Magalie Linda <magaliechetrit@gmail.com>
2021-01-12 16:48:50 -0500

    Little bit of tidying up.

--- footer.php
+++ footer.php
@@ -59,6 +59,6 @@
 </footer>
 
         <?php wp_footer(); ?>
-        <script src="//instant.page/5.1.0" type="module" integrity="sha384-by67kQnR+pyfy8yWP4kPO12fHKRLHZPfEsiSXR8u2IKcTdxD805MGUXBzVPnkLHw"></script>
+        <!-- <script src="//instant.page/5.1.0" type="module" integrity="sha384-by67kQnR+pyfy8yWP4kPO12fHKRLHZPfEsiSXR8u2IKcTdxD805MGUXBzVPnkLHw"></script> -->
     </body>
 </html>
