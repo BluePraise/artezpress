d4637c3ede5e4df7a47782f617fa383623419ff6
Magalie Linda <magaliechetrit@gmail.com>
2021-01-15 17:52:42 -0500

    fix for header and footer

--- inc/templateparts/nav-pages.php
+++ inc/templateparts/nav-pages.php
@@ -1,37 +1,36 @@
 <div class="nav-column">
-            <div class="search">
-                <a href="<?php echo site_url("/books"); ?>" title="Search ArtEZPress Books">Search</a>
-            </div>
-            <ul class="page-list">
-                <li><a href="<?php echo site_url("/"); ?>" title="ArtEZPress">Home</a></li>
-                <li><a href="<?php echo site_url("/books"); ?>" title="ArtEZPress Books">Books</a></li>
-                <li><a href="<?php echo site_url("/news"); ?>" title="ArtEZPress News">News</a></li>
-                <li><a href="<?php echo site_url("/about"); ?>" title="About ArtEZPress">About ArtEZ Press</a></li>
-            </ul>
-            <div class="newsletter">
-                <span>Subscribe to our newsletter</span>
-                <input type="text" />
-                <button type="submit" class="btn btn-reg">OK</button>
+    <div class="search">
+        <a href="<?php echo site_url("/books"); ?>" title="Search ArtEZPress Books">Search</a>
+    </div>
+    <ul class="page-list">
+        <li><a href="<?php echo site_url("/"); ?>" title="ArtEZPress">Home</a></li>
+        <li><a href="<?php echo site_url("/books"); ?>" title="ArtEZPress Books">Books</a></li>
+        <li><a href="<?php echo site_url("/news"); ?>" title="ArtEZPress News">News</a></li>
+        <li><a href="<?php echo site_url("/about"); ?>" title="About ArtEZPress">About ArtEZ Press</a></li>
+    </ul>
+    <div class="newsletter">
+        <span>Subscribe to our newsletter</span>
+        <input type="text" />
+        <button type="submit" class="btn btn-reg">OK</button>
+    </div>
+</div>
+<div class="nav-column">
+    <div class="social-menu">
+        <ul class="horizontal-list">
+            <li>
+            <a href="#" alt="link to facebook"><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/icons/menu_fb.svg" alt="Facebook Icon"></a>
+            <a href="#"><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/icons/menu_ig.svg" alt="Instagram Icon"></a>
+            <a href="#"><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/icons/menu_tw.svg" alt="Twitter Icon"></a>
+        </li></ul>
+    </div>
+    <ul class="page-list">
+        <li><a href="<?php echo site_url("/contact"); ?>" title="Contact ArtEZPress">Contact</a></li>
+        <li><a href="<?php echo site_url("/how-can-we-help"); ?>" title="How Can ArtEZPress Help">How Can We Help?</a></li>
+        <li><a href="<?php echo site_url("/terms-and-conditions"); ?>" title="ArtEZPress Terms and Conditions">Terms and Conditions</a></li>
+        <li><a href="<?php echo site_url("/privacy-policy"); ?>" title="ArtEZPress Privacy Policy">Privacy Policy</a></li>
+        <li><a href="<?php echo site_url("/news"); ?>">Imprint</a></li>
+    </ul>
+    <div class="part-of">ArtEZ Press is part of <br><a href="https://www.artez.nl/" title="link to ArtEZ">ArtEZ University of the Arts</div>
 
-            </div>
-        </div>
-        <div class="nav-column">
-            <div class="social-menu">
-                <ul class="horizontal-list">
-                    <li>
-                    <a href="#" alt="link to facebook"><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/icons/menu_fb.svg" alt="Facebook Icon"></a>
-                    <a href="#"><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/icons/menu_ig.svg" alt="Instagram Icon"></a>
-                    <a href="#"><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/icons/menu_tw.svg" alt="Twitter Icon"></a>
-                </li></ul>
-            </div>
-            <ul class="page-list">
-                <li><a href="<?php echo site_url("/contact"); ?>" title="Contact ArtEZPress">Contact</a></li>
-                <li><a href="<?php echo site_url("/how-can-we-help"); ?>" title="How Can ArtEZPress Help">How Can We Help?</a></li>
-                <li><a href="<?php echo site_url("/terms-and-conditions"); ?>" title="ArtEZPress Terms and Conditions">Terms and Conditions</a></li>
-                <li><a href="<?php echo site_url("/privacy-policy"); ?>" title="ArtEZPress Privacy Policy">Privacy Policy</a></li>
-                <li><a href="<?php echo site_url("/news"); ?>">Imprint</a></li>
-            </ul>
-            <div class="part-of">ArtEZ Press is part of <br><a href="https://www.artez.nl/" title="link to ArtEZ">ArtEZ University of the Arts</div>
-
-        </div>
+</div>
         
