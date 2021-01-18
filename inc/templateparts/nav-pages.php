32bae21246376affe5e51127f47a63017119259e
Magalie Linda <magaliechetrit@gmail.com>
2021-01-18 15:33:11 +0100

    #41 Update Nav-Pages added social icons and added imprint url

--- inc/templateparts/nav-pages.php
+++ inc/templateparts/nav-pages.php
@@ -18,9 +18,9 @@
     <div class="social-menu">
         <ul class="horizontal-list">
             <li>
-            <a href="#" alt="link to facebook"><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/icons/menu_fb.svg" alt="Facebook Icon"></a>
-            <a href="#"><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/icons/menu_ig.svg" alt="Instagram Icon"></a>
-            <a href="#"><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/icons/menu_tw.svg" alt="Twitter Icon"></a>
+            <a href="https://www.facebook.com/ArtezPress/" alt="link to facebook"><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/icons/menu_fb.svg" alt="Facebook Icon"></a>
+            <a href="https://www.instagram.com/artezpress/"><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/icons/menu_ig.svg" alt="Instagram Icon"></a>
+            <a href="https://twitter.com/ArtezPress"><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/icons/menu_tw.svg" alt="Twitter Icon"></a>
         </li></ul>
     </div>
     <ul class="page-list">
@@ -28,7 +28,7 @@
         <li><a href="<?php echo site_url("/how-can-we-help"); ?>" title="How Can ArtEZPress Help">How Can We Help?</a></li>
         <li><a href="<?php echo site_url("/terms-and-conditions"); ?>" title="ArtEZPress Terms and Conditions">Terms and Conditions</a></li>
         <li><a href="<?php echo site_url("/privacy-policy"); ?>" title="ArtEZPress Privacy Policy">Privacy Policy</a></li>
-        <li><a href="<?php echo site_url("/news"); ?>">Imprint</a></li>
+        <li><a href="<?php echo site_url("/imprint"); ?>">Imprint</a></li>
     </ul>
     <div class="part-of">ArtEZ Press is part of <br><a href="https://www.artez.nl/" title="link to ArtEZ">ArtEZ University of the Arts</div>
 
