1aa64008b19959eafe8c214a45f30531b4da1718
Magalie Linda <magaliechetrit@gmail.com>
2021-01-19 21:10:41 +0100

    #47 large text added. added margin on title on page.

--- page.php
+++ page.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 
-<main class="site-main container-xl" role="main">
-<h2><?php the_title(); ?></h2>
-	<article class="content-container">
+<main class="site-main page-default container-xl" role="main">
+	<h2><?php the_title(); ?></h2>
+	<article class="content-container large-text">
 		<?php get_template_part('blocks/index'); ?>
 	</article>
 
