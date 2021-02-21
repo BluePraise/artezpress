d62c4d64984ea44e83ae3a2174f1e1dfe72fe47f
Amir Avraham <74118516+aaamirav@users.noreply.github.com>
2021-02-21 17:30:49 +0100

    Default pages CSS
    - corrected css on all default pages including mobile styles.
    - fixed accordion.
    - minor fixes to 'news item' and 'news archive'

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php get_header(); ?>
 
-<main class="container news" role="main">
+<main class="container-l news" role="main">
     <h2 class="page-title"><?php _e('News', 'storefront'); ?></h2>
     <div class="news-grid-masonry grid">
         <?php
             if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-                <?php get_template_part('inc/templateparts/news', 'excerpt'); ?>	
-            <?php endwhile; 
-            
+                <?php get_template_part('inc/templateparts/news', 'excerpt'); ?>
+            <?php endwhile;
+
         endif; ?>
     </div>
     <?php //the_posts_pagination();?>
