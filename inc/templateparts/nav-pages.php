1b831cc2a0e2bf02f54202fa58235593cdfe23f4
Amir Avraham <74118516+aaamirav@users.noreply.github.com>
2021-02-23 20:07:34 +0100

    Menu css corrections and mobile
    - correction to menu background position (fixed space on top)
    - css corrections on main menu and min-cart
    - fixed the counter notification on cart button
    - changed order of social buttons in the menu (nav-pages.php)
    - removed "search" button in menu on mobile (temporary)
    - fixed footer menu to be identical to header menu (changes in footer.php)
    - menu is now responsive (need to be tested on mobile!)
    - background-colors for mobile menu
    - mobile language toggle
    
    * fixed issue with overflow on flickity

--- inc/templateparts/nav-pages.php
+++ inc/templateparts/nav-pages.php
@@ -15,6 +15,13 @@
     </div>
 </div>
 <div class="nav-column">
+    <ul class="page-list">
+        <li><a href="<?php echo site_url("/contact"); ?>" title="Contact ArtEZPress">Contact</a></li>
+        <li><a href="<?php echo site_url("/how-can-we-help"); ?>" title="How Can ArtEZPress Help"><?php _e('How Can We Help?', 'artezpress');?></a></li>
+        <li><a href="<?php echo site_url("/terms-and-conditions"); ?>" title="ArtEZPress Terms and Conditions"><?php _e('Terms and Conditions', 'artezpress');?></a></li>
+        <li><a href="<?php echo site_url("/privacy-policy"); ?>" title="ArtEZPress Privacy Policy"><?php _e('Privacy Policy', 'artezpress');?></a></li>
+        <li><a href="<?php echo site_url("/imprint"); ?>">Imprint</a></li>
+    </ul>
     <div class="social-menu">
         <ul class="horizontal-list">
             <li>
@@ -23,13 +30,6 @@
             <a href="https://twitter.com/ArtezPress"><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/icons/menu_tw.svg" alt="Twitter Icon"></a>
         </li></ul>
     </div>
-    <ul class="page-list">
-        <li><a href="<?php echo site_url("/contact"); ?>" title="Contact ArtEZPress">Contact</a></li>
-        <li><a href="<?php echo site_url("/how-can-we-help"); ?>" title="How Can ArtEZPress Help"><?php _e('How Can We Help?', 'artezpress');?></a></li>
-        <li><a href="<?php echo site_url("/terms-and-conditions"); ?>" title="ArtEZPress Terms and Conditions"><?php _e('Terms and Conditions', 'artezpress');?></a></li>
-        <li><a href="<?php echo site_url("/privacy-policy"); ?>" title="ArtEZPress Privacy Policy"><?php _e('Privacy Policy', 'artezpress');?></a></li>
-        <li><a href="<?php echo site_url("/imprint"); ?>">Imprint</a></li>
-    </ul>
     <div class="part-of">ArtEZ Press is part of <br><a href="https://www.artez.nl/" title="link to ArtEZ"><?php _e('ArtEZ University of the Arts', 'artezpress');?></a></div>
 
 </div>
