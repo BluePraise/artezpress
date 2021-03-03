685ffcb400a00dd005c0e1194eaa5104a2355129
Magalie Linda <magaliechetrit@gmail.com>
2021-03-03 12:20:38 +0100

    Merge branch 'dev' into designer

--- inc/filter-tags.php
+++ inc/filter-tags.php
@@ -7,24 +7,29 @@
 			</svg>
 		</button>
 	</div>
-
 	<div class="filter-tags__inner js-tags-container">
-		<div class="filter-list filter-tags__list js-filter-elements hide">
+		<div class="filter-list filter-tags__list js-filter-elements filter-tags" style="display:block;">
 			<?php foreach ($terms as $term) : ?>
-				<a href="<?= get_term_link($term->term_id, 'product_tag'); ?>" class="js-filter-item filter-tags__item tag-pill black-on-white" data-filter="product_tag-<?= $term->slug ?>" data-id="<?= $term->term_id ?>"><?= $term->name; ?></a>
+				<a href="<?= get_term_link($term->term_id, 'product_tag'); ?>" class="js-filter-item filter-tags__item tag-pill black-on-white" data-filter="product_tag-<?= $term->slug ?>" data-id="<?= $term->term_id ?>"><?=  _e($term->name); ?></a>
 			<?php endforeach; ?>
 		</div>
-        <div class="filter-list filter-cats__list js-filter-elements show">
+        <div class="filter-list js-filter-elements filter-categories">
             <?php foreach ($categories as $cat) : ?>
-                <a href="" class="js-filter-item sidebar-filters__item tag-pill black-on-white" role="button" data-id="<?= $cat->term_id ?>" data-filter="product_cat-<?= $cat->slug ?>"><?= $cat->name ?></a>
+                <a href="" class="js-filter-item sidebar-filters__item tag-pill black-on-white" role="button" data-id="<?= $cat->term_id ?>" data-filter="product_cat-<?= $cat->slug ?>"><?= _e($cat->name); ?></a>
             <?php endforeach; ?>
         </div>
-        <div class="filter-list filter-year__list js-filter-elements hide" data>
+        <div class="filter-list filter-year__list js-filter-elements filter-years" data-id="<?= $cat->term_id ?>">
             <?php foreach ($years as $year) : ?>
-                <a class="js-filter-item sidebar-filters__item" href="" role="button" data-filter="year-<?= $year ?>"><?= $year ?></a>
+                <a class="js-filter-item sidebar-filters__item tag-pill black-on-white" href="" role="button" data-filter="year-<?= $year ?>"><?= $year ?></a>
+            <?php endforeach; ?>
+        </div>
+        <div class="filter-list filter-language__list js-filter-elements filter-language">
+            <?php foreach ($languages as $lang) : ?>
+                <a class="js-filter-item sidebar-filters__item tag-pill black-on-white" href="" role="button" data-filter="<?= $lang ?>"><?= _e($lang); ?></a>
             <?php endforeach; ?>
         </div>
 	</div>
+
 	<div class="filter-tags__next">
 		<button class="js-tags-next">
 			<svg width="24px" height="24px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
--- inc/filters.php
+++ inc/filters.php
@@ -8,8 +8,9 @@ while (have_posts()) {
   if (strlen(get_field('author'))) {
     $authors[] = explode(',', get_field('author'));
   }
-  if (strlen(get_field('language'))) {
-    $languages[] = explode(',', get_field('language'));
+  if (get_field('ap_language')) {
+    $l = get_field('ap_language');
+    $languages[] = get_field('ap_language');
   }
   if (strlen(get_field('publishing_year'))) {
     $years[] = (int)get_field('publishing_year');
@@ -22,21 +23,22 @@ $years = array_reverse(array_unique($years, SORT_NUMERIC));
 $categories = get_terms(['taxonomy' => 'product_cat']);
 ?>
 <div class="search-bar container-l">
+      
+        <form class="flex main-search js-main-search">
+        <button>
+            <svg width="25px" height="25px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25">
+            <path d="M25 22.57l-6.11-6.11a10.43 10.43 0 10-2.43 2.43L22.57 25zM3.33 10.42a7.09 7.09 0 117.09 7.08 7.1 7.1 0 01-7.09-7.08z" />
+            </svg>
+        </button>
+        <input type="search" class="main-search__input" name="" id="" placeholder="">
+        </form>
+        <div class="filter-section">
+            <div class="filter-header filter-header__tags js-filter-collapse active" data-header="tags"><?php _e('Tags', 'artezpress'); ?></div>
+            <div class="filter-header filter-header__categories js-filter-collapse" data-header="categories"><?= _e('Category', 'artezpress'); ?></div>
+            <div class="filter-header filter-header__year js-filter-collapse" data-header="year"><?= _e('Year', 'artezpress') ?></div>
+            <!-- <div class="filter-header filter-header__language js-filter-collapse" data-header="language"><?php //_e('Language', 'artezpress') ?><</div> -->
+            <div class="filter-header js-reset-filters sidebar-filters__title"><?= _e('All Books', 'artezpress'); ?></div>
+        </div>
+      <?php include get_theme_file_path('/inc/filter-tags.php'); ?>
 
-      <form class="flex-container main-search js-main-search">
-      <button>
-          <svg width="25px" height="25px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25">
-          <path d="M25 22.57l-6.11-6.11a10.43 10.43 0 10-2.43 2.43L22.57 25zM3.33 10.42a7.09 7.09 0 117.09 7.08 7.1 7.1 0 01-7.09-7.08z" />
-          </svg>
-      </button>
-      <input type="search" class="main-search__input" name="" id="" placeholder="">
-      </form>
-      <div class="filter-section">
-          <div class="filter-item js-filter-collapse" data-header="tags">Tags</div>
-          <div class="filter-item js-filter-collapse active" data-header="categories">Category</div>
-          <div class="filter-item js-filter-collapse" data-header="year">Year</div>
-          <div class="filter-item js-filter-collapse" data-header="language">Language</div>
-          <div class="filter-item js-reset-filters sidebar-filters__title">All books</div>
-      </div>
-    <?php include get_theme_file_path('/inc/filter-tags.php'); ?>
 </div>
