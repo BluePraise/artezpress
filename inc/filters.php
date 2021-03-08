55602f7948756387dbbeefbbd8e078b9fd88603e
Magalie Linda <magaliechetrit@gmail.com>
2021-03-08 18:17:51 +0100

    Revert "Merge branch 'designer' into dev"
    This reverts commit ceb85a0b888edc4ae9dd8f27848c8d173e2c1ec1, reversing
    changes made to 8c28a1e4905fc9e3ca99500b3e84ba2df1f18a41.

--- inc/filters.php
+++ inc/filters.php
@@ -22,57 +22,23 @@ $terms = get_terms(array('taxonomy' => 'product_tag'));
 $years = array_reverse(array_unique($years, SORT_NUMERIC));
 $categories = get_terms(['taxonomy' => 'product_cat']);
 ?>
-
-<button class="search-bar__toggle js-search-toggle" type="button" style="display: none;">
-  <div class="search-bar__toggle-icon">
-    <svg width="25px" height="25px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25">
-    <path d="M25 22.57l-6.11-6.11a10.43 10.43 0 10-2.43 2.43L22.57 25zM3.33 10.42a7.09 7.09 0 117.09 7.08 7.1 7.1 0 01-7.09-7.08z" />
-    </svg>
-  </div>
-  <span>Search</span>
-</button>
-
-<div class="search-bar js-scroll-hide">
-  <div class="search-bar__inner container-l ">
-
-    <section class="search-section">
-      <div class="search-section__inner">
-
-        <form class="main-search js-main-search">
-          <button class="main-search__submit">
-            <svg class="main-search__submit-attachment" width="25px" height="25px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25">
+<div class="search-bar container-l">
+      
+        <form class="flex main-search js-main-search">
+        <button>
+            <svg width="25px" height="25px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25">
             <path d="M25 22.57l-6.11-6.11a10.43 10.43 0 10-2.43 2.43L22.57 25zM3.33 10.42a7.09 7.09 0 117.09 7.08 7.1 7.1 0 01-7.09-7.08z" />
             </svg>
-          </button>
-          <input type="search" class="main-search__input js-real-input" name="" placeholder="Search">
+        </button>
+        <input type="search" class="main-search__input" name="" id="" placeholder="">
         </form>
-
-        <form class="main-search fake-typewriter js-fake-typewriter">
-          <button class="main-search__submit">
-            <svg class="main-search__submit-attachment" width="25px" height="25px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25">
-            <path d="M25 22.57l-6.11-6.11a10.43 10.43 0 10-2.43 2.43L22.57 25zM3.33 10.42a7.09 7.09 0 117.09 7.08 7.1 7.1 0 01-7.09-7.08z" />
-            </svg>
-          </button>
-          <div class="main-search__input">
-            <span class="fake-typewriter__input">Search</span>
-            <span class="fake-typewriter__input js-fake-typewriter__input"></span>
-          </div>
-        </form>
-
-      </div>
-    </section>
-
-    <section class="filter-section">
-      <div class="filter-headers-container">
-        <div class="filter-header filter-header__tags active" data-header="tags"><?php _e('Theme', 'artezpress'); ?></div>
-        <div class="filter-header filter-header__categories" data-header="categories"><?= _e('Category', 'artezpress'); ?></div>
-        <div class="filter-header filter-header__year" data-header="year"><?= _e('Year', 'artezpress') ?></div>
-        <div class="filter-header filter-header__language" data-header="languages"><?php _e('Language', 'artezpress') ?></div>
-        <div class="filter-header filter-header__reset js-reset-filters" data-header="reset"><?= _e('Clear', 'artezpress'); ?></div>
-      </div>
-
+        <div class="filter-section">
+            <div class="filter-header filter-header__tags js-filter-collapse active" data-header="tags"><?php _e('Tags', 'artezpress'); ?></div>
+            <div class="filter-header filter-header__categories js-filter-collapse" data-header="categories"><?= _e('Category', 'artezpress'); ?></div>
+            <div class="filter-header filter-header__year js-filter-collapse" data-header="year"><?= _e('Year', 'artezpress') ?></div>
+            <!-- <div class="filter-header filter-header__language js-filter-collapse" data-header="language"><?php //_e('Language', 'artezpress') ?><</div> -->
+            <div class="filter-header js-reset-filters sidebar-filters__title"><?= _e('All Books', 'artezpress'); ?></div>
+        </div>
       <?php include get_theme_file_path('/inc/filter-tags.php'); ?>
-    </section>
-
-  </div>
 </div>
+
