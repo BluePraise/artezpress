57f031993803293aaf10a06c6f719c8ec3d45a1b
Magalie Linda <magaliechetrit@gmail.com>
2021-03-18 18:25:57 +0100

    WIP: checkout

--- woocommerce/checkout/form-checkout.php
+++ woocommerce/checkout/form-checkout.php
@@ -18,20 +18,22 @@
 
 if (!defined('ABSPATH')) {
 	exit;
-}
-
-do_action('woocommerce_before_checkout_form', $checkout);
-
-// If checkout registration is disabled and not logged in, the user cannot checkout.
-if (!$checkout->is_registration_enabled() && $checkout->is_registration_required() && !is_user_logged_in()) {
-	echo esc_html(apply_filters('woocommerce_checkout_must_be_logged_in_message', __('You must be logged in to checkout.', 'woocommerce')));
-	return;
 } ?>
+
+<section class="review-cart-subtotal">
+    
+		<div class="cart-subtotal flex">
+			<div class="subtotal-label"><?php esc_html_e('Subtotal', 'woocommerce'); ?></div>
+			<div class="subtotal-value" data-title="<?php esc_attr_e('Subtotal', 'woocommerce'); ?>"><?php wc_cart_totals_subtotal_html(); ?></div>
+        </div>
+        <?php do_action('woocommerce_review_order_after_cart_contents');  ?>
+</section>
 <form name="checkout" method="post" class="checkout woocommerce-checkout" action="<?php echo esc_url(wc_get_checkout_url()); ?>" enctype="multipart/form-data">
     
 	<?php if ($checkout->get_checkout_fields()) : ?>
 
 		<div class="flex-container woocommerce-checkout-container">
+            
 
 			<div class="col_left">
 				<div class="woocommerce-ap-custom form-title"><?php _e('Billing Address', 'artezpress'); ?></div>
@@ -39,26 +41,23 @@ if (!$checkout->is_registration_enabled() && $checkout->is_registration_required
                     <?php do_action('woocommerce_checkout_after_customer_details'); ?>
 			</div>
 
-		
+            <div class="col_right">
+                <h3 class="woocommerce-ap-custom form-title"> <?php _e("Payment", "woocommerce"); ?></h3>
+                        
+                    <div class="shipping-checkout">
+                        <?php 
+                            // THIS CREATES THE SHIPPING CHOICES. THAT FORM IS BUILT UP IN CHECKOUT/FORM-SHIPPING.PHP
+                            if (WC()->cart->needs_shipping() && WC()->cart->show_shipping()) : ?>
+                            <?php do_action('woocommerce_cart_totals_before_shipping'); ?>
+                            <?php wc_cart_totals_shipping_html(); ?>
+                            <?php do_action('woocommerce_cart_totals_after_shipping'); ?>               
+                            <?php //do_action('woocommerce_checkout_shipping'); ?>
+                        <?php endif; ?>
+                    </div>
 
-		<div class="col_right">
-			<h3 class="woocommerce-ap-custom form-title"> <?php _e("Payment", "woocommerce"); ?></h3>
-                    
-                <div class="shipping-checkout">
-                    <?php 
-                        // THIS CREATES THE SHIPPING CHOICES. THAT FORM IS BUILT UP IN CHECKOUT/FORM-SHIPPING.PHP
-                        if (WC()->cart->needs_shipping() && WC()->cart->show_shipping()) : ?>
-			            <?php do_action('woocommerce_cart_totals_before_shipping'); ?>
-			            <?php wc_cart_totals_shipping_html(); ?>
-			            <?php do_action('woocommerce_cart_totals_after_shipping'); ?>               
-		            <?php endif; ?>
-				    <?php do_action('woocommerce_checkout_shipping'); ?>
-				</div>
+                <?php do_action('woocommerce_after_checkout_form', $checkout); ?>
+            </div>
 
-
-		
-
-			<?php do_action('woocommerce_after_checkout_form', $checkout); ?>
         </div>
     <?php endif; ?>
     
