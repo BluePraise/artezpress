1ec151aacd41a66f3cff15e641c48ca931cccb29
Magalie Chetrit <magaliechetrit@gmail.com>
2021-04-01 18:35:23 +0200

    Only show free shipping when customer orders more than amount

--- functions.php
+++ functions.php
@@ -322,6 +322,28 @@ function veer_popup_maker_gutenburg_compat($content)
  **********************************/
 
 
+/**
+ * Hide shipping rates when free shipping is available.
+ * Updated to support WooCommerce 2.6 Shipping Zones.
+ *
+ * @param array $rates Array of rates found for the package.
+ * @return array
+ */
+function my_hide_shipping_when_free_is_available( $rates ) {
+	$free = array();
+
+	foreach ( $rates as $rate_id => $rate ) {
+		if ( 'free_shipping' === $rate->method_id ) {
+			$free[ $rate_id ] = $rate;
+			break;
+		}
+	}
+
+	return ! empty( $free ) ? $free : $rates;
+}
+
+add_filter( 'woocommerce_package_rates', 'my_hide_shipping_when_free_is_available', 100 );
+
 
 /* Woocommerce filters */
 add_filter( 'wc_add_to_cart_message_html', '__return_false' );
