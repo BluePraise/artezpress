83a4ee84d248707d45fa6532bb865805efcc64f8
Magalie Chetrit <magaliechetrit@gmail.com>
2021-04-01 18:49:59 +0200

    turned on the damn script

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 // --VH variable
 
 // First we get the viewport height and multiple it by 1% to get a value for a vh unit
-// let vh = window.visualViewport.height * 0.01;
+let vh = window.visualViewport.height * 0.01;
 // Set the value in the --vh custom property to the root of the document
-// document.documentElement.style.setProperty('--vh', `${vh}px`);
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 
 jQuery(window).on("load", function () {
