0923e3e95334f25ff5328ecaf8dbedd29d4124e4
Magalie Chetrit <1594036+BluePraise@users.noreply.github.com>
2022-05-08 16:35:07 -0400

    thank you page added styling

--- woocommerce/checkout/thankyou.php
+++ woocommerce/checkout/thankyou.php
@@ -38,18 +38,19 @@ defined( 'ABSPATH' ) || exit;
 			</p>
 
 		<?php else : ?>
-            <h4><?php _e('Your Order Is Confirmed!', 'artezpress'); ?></h4>
 
-			<p class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"><?php echo apply_filters( 'woocommerce_thankyou_order_received_text', esc_html__( 'Thank you. Your order has been received.', 'woocommerce' ), $order ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+			<div class="woocommerce-order-overview woocommerce-thankyou-order-confirmation order_details">
 
-			<div class="woocommerce-order-overview woocommerce-thankyou-order-details order_details">
-
-                <p>Hi <?php echo $billing_name; ?>,</p>
-                <p>Your <?php esc_html_e( 'order No.', 'artezpress' ); ?> <?php echo $order->get_order_number(); esc_html__( ' has been successfully processed. If you have any questions about your order, please contact our support', 'artezpress' )?> <a class="" href="mailto:order@support.hexspoor.nl"><?php _e('customer service', 'artezpress');?></a></p>
-                <p><?php esc_html__( 'Thank you.', 'artezpress');?></p>
+                <?php 
+                    if (is_user_logged_in()) :
+                        $billing_name = get_billing_first_name();
+                    ?>
+                    <p>Hi <?php echo $billing_name; ?>,</p>
+                <?php endif; ?>
+                <p>Your <?php esc_html_e( 'order No.', 'artezpress' ); ?>  <?php echo $order->get_order_number(); esc_html_e( ' has been successfully processed. If you have any questions about your order, please contact our ', 'artezpress' )?> <a class="" href="mailto:artezpress@artez.nl"><?php _e(' customer service (artezpress [at] artez.nl).', 'artezpress');?></a><?php esc_html_e( ' Thank you.', 'artezpress');?></p>
                 <p>ArtEZ Press Team</p>
 			</div>
-			
+
 			<ul class="woocommerce-order-overview woocommerce-thankyou-order-details order_details">
 
 				<li class="woocommerce-order-overview__order order">
@@ -92,6 +93,48 @@ defined( 'ABSPATH' ) || exit;
 
 		<p class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"><?php echo apply_filters( 'woocommerce_thankyou_order_received_text', esc_html__( 'Thank you. Your order has been received.', 'woocommerce' ), null ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
 
+			<div class="woocommerce-order-overview woocommerce-thankyou-order-details order_details">
+
+                <p>Hi <?php echo $billing_name; ?>,</p>
+                <p>Your <?php esc_html_e( 'order No.', 'artezpress' ); ?> <?php echo $order->get_order_number(); esc_html__( ' has been successfully processed. If you have any questions about your order, please contact our support', 'artezpress' )?> <a class="" href="mailto:order@support.hexspoor.nl"><?php _e('customer service', 'artezpress');?></a></p>
+                <p><?php esc_html__( 'Thank you.', 'artezpress');?></p>
+                <p>ArtEZ Press Team</p>
+			</div>
+			
+			<ul class="woocommerce-order-overview woocommerce-thankyou-order-details order_details">
+
+				<li class="woocommerce-order-overview__order order">
+					<?php esc_html_e( 'Order number:', 'woocommerce' ); ?>
+					<strong><?php echo $order->get_order_number(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
+				</li>
+
+				<li class="woocommerce-order-overview__date date">
+					<?php esc_html_e( 'Date:', 'woocommerce' ); ?>
+					<strong><?php echo wc_format_datetime( $order->get_date_created() ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
+				</li>
+
+				<?php if ( is_user_logged_in() && $order->get_user_id() === get_current_user_id() && $order->get_billing_email() ) : ?>
+					<li class="woocommerce-order-overview__email email">
+						<?php esc_html_e( 'Email:', 'woocommerce' ); ?>
+						<strong><?php echo $order->get_billing_email(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
+					</li>
+				<?php endif; ?>
+
+				<li class="woocommerce-order-overview__total total">
+					<?php esc_html_e( 'Total:', 'woocommerce' ); ?>
+					<strong><?php echo $order->get_formatted_order_total(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
+				</li>
+
+				<?php if ( $order->get_payment_method_title() ) : ?>
+					<li class="woocommerce-order-overview__payment-method method">
+						<?php esc_html_e( 'Payment method:', 'woocommerce' ); ?>
+						<strong><?php echo wp_kses_post( $order->get_payment_method_title() ); ?></strong>
+					</li>
+				<?php endif; ?>
+
+			</ul>
+			<?php do_action( 'woocommerce_thankyou_' . $order->get_payment_method(), $order->get_id() ); ?>
+			<?php do_action( 'woocommerce_thankyou', $order->get_id() ); ?>
 	<?php endif; ?>
 
 </div>
