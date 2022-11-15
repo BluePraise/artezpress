e07b4b4ba128096bd5bb22fb27fac9f44fab5dc2
Magalie Chetrit <1594036+BluePraise@users.noreply.github.com>
2022-11-15 12:23:40 +0100

    Added Linkedin to Author

--- archive-authors.php
+++ archive-authors.php
@@ -6,7 +6,7 @@
 <main class="site-main page author-page" role="main">
 
     <h2 class="page-title"><?php echo the_title(); ?></h2>
-		<ul class="container-l grid">
+		<ul class="content-container grid">
 
         <?php if( have_rows('add_author') ): while ( have_rows('add_author') ) : the_row(); ?>
 
@@ -27,6 +27,9 @@
                  <?php endif; ?>
                  <?php if (get_sub_field('author_facebook')): ?>
                     <a href="<?php echo the_sub_field('author_facebook'); ?>" class="link-list__item author-facebook" alt="link to facebook"><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/icons/menu_fb.svg" alt="Facebook Icon"></a>
+                 <?php endif; ?>
+                  <?php if (get_sub_field('author_linkedin')): ?>
+                    <a href="<?php echo the_sub_field('author_linkedin'); ?>" class="link-list__item author-linkedin"  alt="link to linkedin"><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/icons/menu_li.svg" alt="LinkedIn Icon"></a>                   
                  <?php endif; ?>
                  <?php if (get_sub_field('author_website')): ?>
                     <a href="<?php echo the_sub_field('author_website'); ?>" class="link-list__item author-website"  alt="link to website"><img src="<?php echo get_stylesheet_directory_uri() ?>/assets/icons/menu_link.svg" alt="Link Icon"></a>                   
